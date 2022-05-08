f474b4c7c2a85e2318d5bf45202b57d93d00932b
Abhiram Shibu <abhiramshibu1998@gmail.com>
2022-05-09 00:54:41 +0530

    AnonChat: Checksum save, load/save file AnonFS

--- AnonFS_Interface.py
+++ AnonFS_Interface.py
@@ -7,20 +7,27 @@ def anonfs_interface(host, port, restore=False):
         cwd = os.getcwd()
         file_path = os.path.join(cwd,"messages.log")
         if(restore):
+            print("Restore Request Received")
             # load checksum from file
             checksum = open("checksum.txt","r").read()
+            # log
+            print("Checksum:",checksum)
+            print("Sending "+f"download {file_path} {checksum}")
             s.send(f"download {file_path} {checksum}\n".encode())
+            print("Waiting for DONE Acknowledgement")
+            data = s.recv(1024).decode()
+            if(data == "DONE"):
+                print("Restore Successful")
+            else:
+                print("Restore Failed")
         else: 
+            print("Upload Request Received")
             s.send(f"upload {file_path}\n".encode())
+            print("Upload Request Completed")
             checksum = s.recv(1024)
+            print("Received Checksum:",checksum)
             # save checksum to file
             with open("checksum.txt", "w") as f:
                 f.write(checksum.decode())
-            # get checksum from file
-            with open("checksum.txt", "r") as f:
-                checksum_file = f.read()
-
-
-
-            
+            print("Checksum Saved")
             
